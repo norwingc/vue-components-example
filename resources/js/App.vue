aafbd7003d7068b88f1343ba48eb5ef51a907ef8
Norwin Guerreo <norwingcruz@gmail.com>
2019-02-18 22:09:02 -0500

    child to parent

---
+++ resources/js/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <first-component v-on:childToParent="getDataOne"></first-component>
+        <br>
+        <pre class="m-5">{{ persona }}</pre>
+    </div>
+</template>
+
+<script>
+import FirstComponent from './components/FirstComponent.vue'
+
+export default {
+    components:{
+        'first-component' : FirstComponent
+    },
+    data(){
+        return{
+            persona: {}
+        }
+    },
+     methods:{
+            getDataOne (value){
+                this.persona = value
+            }
+        }
+}
+</script>
---
+++ resources/js/components/FirstComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="m-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur voluptatem delectus voluptate sit debitis consequuntur, similique, labore rem molestiae autem aspernatur quasi ad quod ipsa rerum nesciunt, tenetur maxime? Sunt!</p>
+        <input type="text" v-model="persona.first_name">
+        <input type="text" v-model="persona.last_name">
+
+        <button @click="sendData">Save</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                persona:{
+                    first_name: 'jlfasdf',
+                    last_name: 'afsdf'
+                }
+            }
+        },
+        methods:{
+            sendData(){
+               this.$emit('childToParent', this.persona)
+            }
+        }
+    }
+</script>
